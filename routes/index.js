73b3222175a115950b3a81a04fa5eced7ad3edde
Belen Curcio <curciobelen@gmail.com>
2017-04-19 16:08:12 -0300

    Ádding routes, and views for login

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.use('/admin', require('./admin'));
 router.use('/embed', require('./embed'));
 router.get('/embed.js', (req, res) => res.sendFile(path.join(__dirname, '../dist/embed.js')));
 router.get('/embed.js.map', (req, res) => res.sendFile(path.join(__dirname, '../dist/embed.js.map')));
+router.get('/login', (req, res) => res.render('login'));
 
 if (process.env.NODE_ENV !== 'production') {
   router.use('/assets', require('./assets'));
